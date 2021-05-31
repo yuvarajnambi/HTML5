54552dd067039f258cc6a9dca005bb04b7a7e26b
yuvarajnambi <85125089+yuvarajnambi@users.noreply.github.com>
2021-05-31 21:53:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function NEXTFunction()
+{
+    alert('Alert!Go Next');
+}
+
+function BACKFunction() {
+    alert('Alert!Go Back');
+}
+function ExitFunction() {
+    alert('Alert! Exit the course');
+}
